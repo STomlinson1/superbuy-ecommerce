18738aefe1a14fe89650b2b134de6b4635beca1a
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-10-02 00:08:14 -0400

    Set up products routes

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const products = require('./data/products');
+
+const app = express();
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+	res.send('Server is running');
+});
+
+app.get('/api/products', (req, res) => {
+	res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+	const product = products.find((product) => product._id === req.params.id);
+	res.json(product);
+});
+
+app.listen(5000, console.log('Server running on port 5000'));
