fbf5079eb6d697079c1c7953a74399d19b84d3a3
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-10-01 22:04:12 -0400

    React Router Links

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
 	return (
@@ -13,6 +14,7 @@ const App = () => {
 			<Container>
 				<main>
 					<Route exact path="/" component={HomeScreen} />
+					<Route exact path="/product/:id" component={ProductScreen} />
 				</main>
 			</Container>
 			<Footer />
