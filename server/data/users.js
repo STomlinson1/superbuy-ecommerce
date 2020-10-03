656c6cf964e3473bd2c487f67a9d90f778c07372
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-10-03 14:00:12 -0400

    Created sample data for users

---
+++ server/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+	{
+		name     : 'Admin User',
+		email    : 'admin@example.com',
+		password : bcrypt.hashSync('123456', 10),
+		isAdmin  : true
+	},
+	{
+		name     : 'Alfred Ortiz',
+		email    : 'admin@example.com',
+		password : bcrypt.hashSync('123456', 10)
+	},
+	{
+		name     : 'John Gustafson',
+		email    : 'admin@example.com',
+		password : bcrypt.hashSync('123456', 10)
+	}
+];
+
+export default users;
