a660eab2135112e25234077623c1a80cb6b3ee42
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-10-02 20:55:08 -0400

    Created user and product models.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
+import colors from 'colors';
 
 import products from './data/products.js';
 import connectDB from './config/db.js';
@@ -30,5 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
 );
