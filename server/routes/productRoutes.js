ee76a7eb2e3e132a46286e2b559607290114f55f
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-10-03 16:09:45 -0400

    Added Error Handling middleware.

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,6 +4,9 @@ import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
 router.get(
 	'/',
 	asyncHandler(async (req, res) => {
@@ -12,7 +15,21 @@ router.get(
 	})
 );
 
-router.get('/:id', (req, res) => {
-	const product = products.find((product) => product._id === req.params.id);
-	res.json(product);
-});
+// @desc Fetch a single product
+// @route GET /api/products/:id
+// @access Public
+router.get(
+	'/:id',
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+
+		if (product) {
+			res.json(product);
+		} else {
+			res.status(404);
+			throw new Error('Product not found');
+		}
+	})
+);
+
+export default router;
