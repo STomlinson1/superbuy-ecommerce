4e54858aad27643f03e73ad23cebbba730691ac5
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-10-03 22:23:31 -0400

    Message and Loader components

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -16,8 +16,8 @@ export const listProducts = () => async (dispatch) => {
 		dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
 	} catch (error) {
 		dispatch({
-			type: PRODUCT_LIST_FAIL,
-			payload:
+			type    : PRODUCT_LIST_FAIL,
+			payload :
 				error.response && error.response.data.message
 					? error.response.data.message
 					: error.message
--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,30 +1,44 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { Fragment, useEffect } from 'react';
+import {
+	useDispatch, // Used to call an action
+	useSelector // Used to select parts of the state
+} from 'react-redux';
+import { listProducts } from '../actions/productActions';
+
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 
 const HomeScreen = () => {
-	const [ products, setProducts ] = useState([]);
+	const dispatch = useDispatch(); //define dispatch
 
-	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products');
-			setProducts(data);
-		};
+	// Get the productList state from the Redux store
+	const productList = useSelector((state) => state.productList);
 
-		fetchProducts();
-	}, []);
+	const { loading, error, products } = productList;
+
+	useEffect(
+		() => {
+			dispatch(listProducts()); //dispatch the listProducts action
+		},
+		[ dispatch ]
+	);
 
 	return (
 		<Fragment>
 			<h1>Latest Products</h1>
-			<Row>
-				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<h3>{error}</h3>
+			) : (
+				<Row>
+					{products.map((product) => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</Fragment>
 	);
 };
