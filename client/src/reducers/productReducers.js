423242b25bc562410d596abadbc68156c71f012c
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-10-04 16:08:12 -0400

    Clean up product reducer

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -20,14 +20,17 @@ export const productListReducer = (state = { products: [] }, action) => {
 	}
 };
 
-export const productDetailReducer = (state = { product: {} }, action) => {
+export const productDetailReducer = (
+	state = { product: { reviews: [] } },
+	action
+) => {
 	switch (action.type) {
 		case PRODUCT_DETAILS_REQUEST:
-			return { loading: true, product: {} };
+			return { ...state, loading: true };
 		case PRODUCT_DETAILS_SUCCESS:
 			return { loading: false, product: action.payload };
 		case PRODUCT_DETAILS_FAIL:
-			return { loading: false, product: {}, error: action.payload };
+			return { loading: false, error: action.payload };
 		default:
 			return state;
 	}
